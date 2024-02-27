110e6865772b660b0a12bdd79e9ad6762bae445d
bastian6666 <57109930+bastian6666@users.noreply.github.com>
2024-02-26 20:23:59 -0500

    Update package dependencies and install rMR package

--- Main.R
+++ Main.R
@@ -1,10 +1,10 @@
 ### Aquatic Forecast Workflow ###
-#devtools::install_github("eco4cast/neon4cast")
+devtools::install_github("eco4cast/neon4cast")
 library(tidyverse)
-#library(neon4cast)
+library(neon4cast)
 library(lubridate)
-#install.packages("rMR")
-#library(rMR)
+install.packages("rMR")
+library(rMR)
 
 forecast_date <- Sys.Date()
 noaa_date <- Sys.Date() - days(1)  #Need to use yesterday's NOAA forecast because today's is not available yet
