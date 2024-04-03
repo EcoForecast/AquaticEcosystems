7ba795b78e72c630bdc4288bddec550ab1b607d4
bastian6666 <57109930+bastian6666@users.noreply.github.com>
2024-04-02 22:14:34 -0400

    DLM forecasting process

--- DLM.R
+++ DLM.R
@@ -8,7 +8,7 @@ library(readr)
 library(rjags)
 #library(rnoaa)
 library(daymetr)
-#devtools::install_github("EcoForecast/ecoforecastR",force=TRUE)
+# devtools::install_github("EcoForecast/ecoforecastR",force=TRUE)
 library(ecoforecastR)
 library(zoo)
 library(padr)
@@ -24,14 +24,14 @@ site_data <- readr::read_csv("https://raw.githubusercontent.com/eco4cast/neon4ca
 
 
 site_data <- site_data |> 
-  dplyr::filter(aquatics == 2)
+  dplyr::filter(aquatics == 1)
 
 site_ids <- site_data$field_site_id
 
 # Load historical data using neon4cast
-df_past <- neon4cast::noaa_stage3()
+# df_past <- neon4cast::noaa_stage3()
 
-site = "ARIK"
+site = "BARC"
 
 site_ox <- oxygen |> 
   dplyr::filter(site_id == site)
@@ -57,7 +57,11 @@ data <- list(y=y,n=length(y),      ## data
              temp = temp
 )
 
-ef.out <- ecoforecastR::fit_dlm(model=list(obs="y",fixed="~ 1 + X + temp"),data)
+#model = list(obs="y",fixed="~ 1 + X + temp", n.iternumber = 20000)
+
+model = list(obs="y",fixed="~ 1 + X", n.iternumber = 20000)
+
+ef.out <- ecoforecastR::fit_dlm(model=model,data)
 
 params <- window(ef.out$params,start=1000)
 plot(params)
@@ -74,3 +78,48 @@ plot(time,ci[2,],
      )
 ecoforecastR::ciEnvelope(time,ci[1,],ci[3,],col=ecoforecastR::col.alpha("lightBlue",0.75))
 points(time,y,pch="+",cex=0.5)
+
+
+
+#### Milestone 6 ####
+
+
+forecastN <- function(IC ,betaIntercept, betaX ,Q=0,n=Nmc){
+
+  NT = 30
+  N <- matrix(NA,n,NT)  ## storage
+  Nprev <- IC           ## initialize
+  for(t in 1:NT){
+    mu = Nprev + betaX * Nprev + betaIntercept ## mean
+    N[,t] <- rnorm(n,mu,Q)                         ## predict next step
+    Nprev <- N[,t]                                  ## update IC
+  }
+  return(N)
+}
+
+## parameters
+params <- as.matrix(ef.out$params)
+param.mean <- apply(params,2,mean)
+
+IC <- as.matrix(ef.out$predict)
+
+
+forecast <- forecastN(mean(IC[,"x[1651]"]), param.mean[1], param.mean[2], Q = 0, n = 1)
+NT = 30
+time1 = 1:NT
+
+
+x = 1:length(ci[2,])
+
+time2 = length(x):(length(x)+NT-1)
+
+plot(x,ci[2,],
+     type='n',
+     ylim=range(y,na.rm=TRUE),
+     ylab="Dissolved Oxygen",
+     xlim = c(1500,length(ci[2,]) + NT),
+     )
+ecoforecastR::ciEnvelope(x,ci[1,],ci[3,],col=ecoforecastR::col.alpha("lightBlue",0.75))
+points(x,y,pch="+",cex=0.5)
+
+lines(time2,forecast,col="purple",lwd=3)
